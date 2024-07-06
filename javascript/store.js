c29bf9810a68e569f0811911735875015b4a1815
shahin-alm06 <adnanshahin0011@gmail.com>
2024-07-06 22:37:35 +0600

    api added the main things

---
+++ javascript/store.js
@@ -0,0 +1,39 @@
+function handlesearch() {
+    const searchElement = document.getElementById('search-input-field');
+    const searchInputValue = searchElement.value;
+    console.log(searchInputValue);
+    loadPhone(searchInputValue);
+}
+
+const loadPhone = async(searchText) => { 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    console.log("Server Response", res);
+    const serverData = await res.json();
+    displayPhone (serverData.data);
+
+};
+
+const displayPhone = (data) => {
+    console.log(data);
+    const cardContainer = document.getElementById('card-section');
+    cardContainer.innerHTML = "";
+    data.forEach(phone => {
+        const productCard = document.createElement('div');
+        productCard.classList.add("card");
+        productCard.innerHTML = `
+        <div class="card-image">
+                    <img src=${phone.image} alt="phone image"/>
+                </div>
+                <h3 class="card-title">${phone.phone_name}</h3>
+                <p class="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, doloribus?</p>
+                <div class="card-price">
+                    <span>$</span>
+                    <span id="card-item-price">990</span>
+                </div>
+                <div class="card-button">
+                    <button class="btn">Show Details</button>
+                </div>
+        `;
+        cardContainer.appendChild(productCard);
+    });
+};
